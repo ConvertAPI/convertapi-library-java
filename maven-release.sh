7337c7d911a6d120f16afec51bbd7967359dbd2c
Krisztián Mózsi <mozsik0@gmail.com>
2019-07-18 22:46:08 +0200

    Add maven release script

---
+++ maven-release.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+branch_to_release="feature/maven"
+
+echo "Switching to the release branch..."
+git fetch
+git checkout $branch_to_release
+
+echo "Pulling changes..."
+git pull --ff-only
+
+echo "Push commits..."
+git push
+
+echo "Revert uncommitted changes..."
+git checkout .
+
+echo "Cleaning..."
+mvn clean
+
+echo "Preparing release..."
+if [ $# -eq 1 ]
+then
+	echo -en "\n\n" | mvn release:prepare -Dtag=$1
+elif [ $# -eq 2 ] 
+then
+	echo -en "\n" | mvn release:prepare -DdevelopmentVersion=$2 -Dtag=$1
+elif [ $# -eq 3 ] 
+then
+	mvn release:prepare -DreleaseVersion=$3 -DdevelopmentVersion=$2 -Dtag=$1
+else
+	echo -en "\n\n\n" | mvn release:prepare
+fi
+
+echo "Performing release..."
+mvn release:perform
+
+echo "Push the new release tag..."
+git push --tags
+
+echo "Push release commits..."
+git push
