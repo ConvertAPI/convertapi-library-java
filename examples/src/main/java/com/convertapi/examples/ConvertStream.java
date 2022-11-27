349c83c87d61065e34e2773f3d3fa7f987f8e619
Krisztian Mozsi <krisztianmozsi@gmail.com>
2022-11-27 01:54:20 +0000

    Format fixes

--- examples/src/main/java/com/convertapi/examples/ConvertStream.java
+++ examples/src/main/java/com/convertapi/examples/ConvertStream.java
@@ -6,9 +6,9 @@ import com.convertapi.client.ConvertApi;
 import com.convertapi.client.Param;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.CompletableFuture;
@@ -25,7 +25,7 @@ public class ConvertStream {
         Config.setDefaultSecret(getenv("CONVERTAPI_SECRET"));    //Get your secret at https://www.convertapi.com/a
 
         // Creating file data stream
-        InputStream stream = new FileInputStream(new File("src/main/resources/test.docx"));
+        InputStream stream = Files.newInputStream(new File("src/main/resources/test.docx").toPath());
 
         System.out.println("Converting stream of DOCX data to PDF");
         CompletableFuture<ConversionResult> result = ConvertApi.convert("docx", "pdf",
